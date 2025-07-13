4985dff5b5f9ed09c5cd4cd0df174f9ca7e7e7e8
carbonsoda <carbonsoda@outlook.com>
2025-07-13 15:34:29 +0800

    finished algorithm

---
+++ src/gradupe.py
@@ -0,0 +1,50 @@
+import concurrent.futures as fu
+import itertools as it
+import math
+
+import cv2 as cv
+import numpy as np
+
+type Image = np.ndarray
+type Sobel = tuple[np.ndarray, np.ndarray]
+
+
+def read_image(image_path: str, resolution: int) -> Image:
+    image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+
+    return cv.resize(image, (resolution, resolution))
+
+
+def image_sobel(image: Image) -> Sobel:
+    gx, gy = cv.spatialGradient(image)
+
+    return gx > 0, gy > 0
+
+
+def image_dist(sobel1: Sobel, sobel2: Sobel) -> float:
+    gx1, gy1 = sobel1
+    gx2, gy2 = sobel2
+
+    dx = (gx1 ^ gx2).sum()
+    dy = (gy1 ^ gy2).sum()
+
+    return math.hypot(dx, dy)
+
+
+def image_dupes(
+    image_paths: list[str], resolution: int, threshold: float
+) -> list[tuple[str, str]]:
+    max_dist = pow(resolution, 2) * math.sqrt(2) * threshold
+
+    with fu.ThreadPoolExecutor() as exe:
+        grads = exe.map(
+            lambda path: image_sobel(read_image(path, resolution)), image_paths
+        )
+
+    return [
+        (image1_path, image2_path)
+        for (image1_path, sobel1), (image2_path, sobel2) in it.combinations(
+            zip(image_paths, grads), 2
+        )
+        if image_dist(sobel1, sobel2) < max_dist
+    ]
---
+++ src/main.py
@@ -0,0 +1,48 @@
+import pathlib
+
+import imagesize
+import gradupe
+
+
+def ask(item: str, default: str) -> str:
+    return input(f"{item.ljust(30)} ({default.center(20)}) : ") or default
+
+
+def main():
+    images_path = pathlib.Path(ask("images path", "/"))
+    images_exts = ask("images exts", "jpeg|jpg|png").split("|")
+
+    resolution = int(ask("resolution", "9"))
+    threshold = float(ask("threshold", ".08"))
+
+    image_paths = [
+        str(path)
+        for path in images_path.iterdir()
+        if path.suffix[1:].lower() in images_exts
+    ]
+
+    image_dupes = gradupe.image_dupes(image_paths, resolution, threshold)
+
+    for image_dupe in image_dupes:
+        image1_path, image2_path = image_dupe
+
+        image1_size, image2_size = (
+            imagesize.get(image1_path),
+            imagesize.get(image2_path),
+        )
+
+        print(
+            f"{str(image1_size).ljust(15)} {image1_path}",
+            f"{str(image2_size).ljust(15)} {image2_path}",
+            sep="\n",
+            end="\n\n",
+        )
+
+    print(
+        f"found {len(image_dupes)} ({len(image_dupes) * 2}) dupes out of {len(image_paths)} images, "
+        f"resolution {resolution} @ threshold {threshold}"
+    )
+
+
+if __name__ == "__main__":
+    main()
